ef09b41d50cd666f72adeeee0aafa01f310d99e3
saitejach127 <saitejach127@gmail.com>
2020-12-31 15:46:35 +0530

    calling code added

---
+++ index.js
@@ -0,0 +1,28 @@
+const web3 = require("web3");
+require("dotenv");
+
+// You can convert functions to async await based on requirements but do not change the structure
+
+let deployedContractAddress = null;
+let publickey = process.env.publickey;
+let privatekey = process.env.privatekey
+
+function connectWeb3(){
+    // Todo : Connect web3 to rinkeby network
+}
+
+function deployContract(){
+    //Todo : Deploy the contract
+}
+
+function callAddCandidate(candidateName){
+    // Todo : calling the addCandidate function in contract
+}
+
+function callVote(candidateName){
+    //Todo : calling the vote function in contract with senders address
+}
+
+function callEnd(){
+    //Todo : call end function in contract
+}
