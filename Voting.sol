22f9d4d96115692863141c7f314ceae9c3c39738
sahilkumar25 <ramagirisahil@gmail.com>
2021-01-04 19:39:16 +0530

    added endtime to election based on duration

--- Voting.sol
+++ Voting.sol
@@ -8,6 +8,7 @@ contract Voting {
     mapping(uint256 => uint256) voteCount; // To keep track of votes for candidate (candidate id is used for noting which is index of candidates array)
     mapping(address => bool) voted; // use voted or not
     bool ended = false; // To check ended oor not
+    uint endtime;
     modifier onlyAdmin{
         // Todo : Only adming modifier
         require(msg.sender == admin);
@@ -16,13 +17,14 @@ contract Voting {
     
     modifier notEnded{
         //Todo : Verify ended or not
-        require(ended == false);
+        require(ended == false && block.timestamp < endtime);
         _;
     }
     
-    constructor (){
+    constructor (uint duarationHours){
         // Todo : Assign admin address to contract creaters address
         admin=msg.sender;
+        endtime =  block.timestamp + (duarationHours * 1 hours);
     }
     
     function addCandidate(string memory _candidateName) onlyAdmin notEnded public {
